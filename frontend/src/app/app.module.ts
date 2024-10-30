db9eb89427086a95432b804b62e757f511a0a38f
Subir Mahapatra <subirmahapatra809@gamil.com>
2024-10-30 20:57:38 +0530

    This is file and folder structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { MovieComponent } from './template/movie/movie.component';
+import { MovieDetailsComponent } from './template/movie/movieDetails/movie-details/movie-details.component';
+import { MovieListComponent } from './template/movie/movie-list/movie-list.component';
+import { SharedModule } from './shared/shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MovieComponent, MovieDetailsComponent, MovieListComponent],
   imports: [
+    SharedModule,
+    BrowserModule
   ],
   providers: [
   ],
