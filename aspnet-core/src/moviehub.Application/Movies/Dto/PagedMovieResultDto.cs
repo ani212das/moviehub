b95060a2b769631e2ebe11325b4a22f87556cbc4
Anirban <anirban212das@gmail.com>
2024-11-10 02:05:21 +0530

    added movie CRUD

---
+++ aspnet-core/src/moviehub.Application/Movies/Dto/PagedMovieResultDto.cs
@@ -0,0 +1,13 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moviehub.Movies.Dto
+{
+    public class PagedMovieResultDto : PagedResultDto<MovieDto>
+    {
+    }
+}
---
+++ aspnet-core/src/moviehub.Application/Movies/IMovieAppService.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Application.Services;
+using moviehub.Movies.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moviehub.Movies
+{
+    public interface IMovieAppService : IApplicationService
+    {
+        Task CreateOrUpdateMovieAsync(CreateOrUpdateMovieDto input);
+        Task<MovieDto> GetMovieAsync(long id);
+        Task<PagedMovieResultDto> GetPagedMoviesAsync(int skipCount, int maxResultCount);
+        Task DeleteMovieAsync(long id);
+    }
+}
