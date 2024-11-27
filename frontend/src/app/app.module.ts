9262dded3398486e21be8c52d4484bfe149a9a29
Subir Mahapatra <subirmahapatra809@gamil.com>
2024-11-27 12:48:00 +0530

    here i will create a all type of component and services.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AdminListComponent } from './template/admin-list/admin-list.component';
+import { UserListComponent } from './template/user-list/user-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AdminListComponent, UserListComponent],
   imports: [
     AppRoutingModule
   ],
---
+++ frontend/src/app/template/list-item.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ListItemRoutingModule } from './list-item-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ListItemRoutingModule
+  ]
+})
+export class ListItemModule { }
