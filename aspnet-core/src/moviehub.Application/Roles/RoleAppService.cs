845b53f27f5c845e0f2597d27ca8c97e92a3844c
Subha8371 <janasubhas9735@gmail.com>
2024-12-05 02:41:59 +0530

    user crud with pagination,search,filter

--- aspnet-core/src/moviehub.Application/Roles/RoleAppService.cs
+++ aspnet-core/src/moviehub.Application/Roles/RoleAppService.cs
@@ -17,7 +17,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace moviehub.Roles
 {
-    [AbpAuthorize(PermissionNames.Pages_Roles)]
+    //[AbpAuthorize(PermissionNames.Pages_Roles)]
     public class RoleAppService : AsyncCrudAppService<Role, RoleDto, int, PagedRoleResultRequestDto, CreateRoleDto, RoleDto>, IRoleAppService
     {
         private readonly RoleManager _roleManager;
